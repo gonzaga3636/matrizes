ef61afd8744fdf78c14e827417ae66e669c55754
gonzaga <luanpaolini01@gmail.com>
2025-05-22 20:15:52 -0300

    Create matriz5.c

---
+++ matriz5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    
+    int M[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}}, i, j, soma = 0;
+    
+       
+       for(i = 0; i < 4; i++){
+           for(j = 3; j >= 3 - i ; j--){
+               soma += M[i][j];
+           }
+          }
+       
+    printf("Resultado: %d", soma);
+    
+    
+    return 0;
+}
